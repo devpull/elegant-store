bb766144ecec1b3e435133415ca8f3d2d4a5ee9d
devpull <devpull@yandex.ru>
2024-10-08 22:08:39 +0300

    Swiper starter.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
 import tagline from "./modules/tagline";
+import Swiper from "swiper";
+import { Navigation, Pagination } from 'swiper/modules';
+
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  modules: [Navigation, Pagination],
+  simulateTouch: true,
+  slideClass: 'card',
+  slidesPerView: 'auto',
+
+  pagination: {
+    el: '.dots',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
