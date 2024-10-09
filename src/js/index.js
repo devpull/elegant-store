ff6cd893e576daca419bb8d16c57e73005587a01
devpull <devpull@yandex.ru>
2024-10-09 18:25:22 +0300

    Arrivals swiper done.

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,2 @@
-import tagline from "./modules/tagline";
-import Swiper from "swiper";
-import { Navigation, Pagination } from 'swiper/modules';
-
-import "swiper/css";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  modules: [Navigation, Pagination],
-  simulateTouch: true,
-  slideClass: 'card',
-  slidesPerView: 'auto',
-
-  pagination: {
-    el: '.dots',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+import "./modules/tagline";
+import "./swiper"
---
+++ src/js/swiper.js
@@ -0,0 +1,30 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  modules: [Navigation, Pagination],
+  simulateTouch: true,
+  slideClass: "card",
+  slidesPerView: "auto",
+  slidesPerGroupAuto: true,
+  spaceBetween: 24,
+
+  pagination: {
+    el: ".dots",
+    horizontalClass: "dots--horizontal",
+    bulletClass: "dots__dot",
+    bulletActiveClass: "dots__dot--active",
+    clickableClass: "dots--clickable",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
