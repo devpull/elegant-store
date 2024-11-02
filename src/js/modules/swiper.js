612c56356f8f2d4325ddbd9c546fe5196f5da518
devpull <devpull@yandex.ru>
2024-11-02 17:17:47 +0300

    Removed css from bundle. Imported scss from node_modules for swiper.

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,8 +1,8 @@
 import Swiper from "swiper";
 import { Pagination } from "swiper/modules";
 
-import "swiper/css";
-import "swiper/css/pagination";
+// import "swiper/css";
+// import "swiper/css/pagination";
 
 const swiper = new Swiper(".swiper", {
   // Optional parameters
